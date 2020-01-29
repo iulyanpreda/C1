c3e5b7250039b439cffda9a78fd96f8940cf8d6e
Iulian Preda <iulyanpreda@yahoo.com>
2020-01-29 21:35:17 +0200

    z

---
+++ Zile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for a :");
+            string x = Console.ReadLine();
+            switch (x)
+            {
+                case "1":
+                    Console.WriteLine("Monday");
+                    break;
+                case "2":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "3":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "4":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "5":
+                    Console.WriteLine("Friday");
+                    break;
+                case "6":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "7":
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
