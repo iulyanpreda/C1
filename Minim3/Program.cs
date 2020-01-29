19431068d8feb7c02e962d36a79ba7c84e84f8bf
Iulian Preda <iulyanpreda@yahoo.com>
2020-01-29 21:18:50 +0200

    m

---
+++ Minim3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Minim3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for a :");
+            string x = Console.ReadLine();
+            int a = int.Parse(x);
+            Console.WriteLine("Enter value for b :");
+            string y = Console.ReadLine();
+            int b = int.Parse(y);
+            Console.WriteLine("Enter value for c :");
+            string z = Console.ReadLine();
+            int c = int.Parse(z);
+            if (a < b && a < c)
+            {
+                Console.WriteLine("a " + a + " is minim");
+            }
+            else
+            {
+                if (b < c && b < a)
+                {
+                    Console.WriteLine("b " + b + " is minim");
+                }
+                else
+                {
+                    Console.WriteLine("c " + c + " is minim");
+
+                }
+
+            }
+
+            Console.Read();
+        }
+
+
+    }
+}
