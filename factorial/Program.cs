019a9d8230012a2b464c2c56922bfc7ddb56c91b
Iulian Preda <iulyanpreda@yahoo.com>
2020-01-29 21:06:55 +0200

    f1

--- factorial/Program.cs
+++ factorial/Program.cs
@@ -2,11 +2,32 @@
 
 namespace factorial
 {
-    class Program
-    {
-        static void Main(string[] args)
+	public class Program
+	{
+		public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fact = 1;
+            Console.WriteLine("Enter value for n :");
+            string x = Console.ReadLine();
+            int n = int.Parse(x) ;
+            if ( n > 13 || n < 0)
+            {
+                Console.WriteLine("Enter a number beetween 0 and 13");
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                    {
+                    fact = fact * i;
+                    }
+                Console.WriteLine("N factorial is: " + fact);
+
+            }
+
+            Console.Read();
         }
+
+
     }
 }
+
