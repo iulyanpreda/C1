3f9eeb014e76402b63bbadba38ed52be293d3a6a
Iulian Preda <iulyanpreda@yahoo.com>
2020-01-29 21:26:40 +0200

    i

---
+++ ParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            string x = Console.ReadLine();
+            int n = int.Parse(x);
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number " + n + "  is even");
+            }
+            else
+            {
+                Console.WriteLine("The number " + n + " is odd");
+            }
+        }
+    }
+}
