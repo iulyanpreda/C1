541388faba637da3a97f376b9a42e3dee6a8eae7
Iulian Preda <iulyanpreda@yahoo.com>
2020-02-23 19:30:57 +0200

    z

---
+++ Tema3/Ex5T3_oddToUpper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex5T3_oddToUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] v = new char[] { 'a', 'a', 'b','b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f' };
+            char[] v1 = new char[v.Length];
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    v1[i] = char.ToUpper(v[i]);
+                    // Console.WriteLine(char.ToUpper(v[i]));
+                        }
+                else
+                {
+                    v1[i] = v[i];
+                    //Console.WriteLine(v[i]);
+                }
+
+                    Console.WriteLine(v1[i]);
+
+                
+
+
+            }
+        }
+    }
+}
