803679eade10b4a595838635365795c4cc174ac8
Iulian Preda <iulyanpreda@yahoo.com>
2020-02-23 17:51:25 +0200

    changes

--- Tema3/Ex1T3_parcurgereVector/Program.cs
+++ Tema3/Ex1T3_parcurgereVector/Program.cs
@@ -6,7 +6,9 @@ namespace Ex1T3_parcurgereVector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] v = new int[] {15,6,17};
+            for (int i = 0; i < v.Length; i++)
+                Console.WriteLine(v[i]);
         }
     }
 }
---
+++ Tema3/Ex2T3_minMaxVector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex2T3_minMaxVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
