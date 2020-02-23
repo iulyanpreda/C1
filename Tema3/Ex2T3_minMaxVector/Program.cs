14025998d05bdf480b813e357b86b72c39e36015
Iulian Preda <iulyanpreda@yahoo.com>
2020-02-23 18:28:13 +0200

    z

--- Tema3/Ex2T3_minMaxVector/Program.cs
+++ Tema3/Ex2T3_minMaxVector/Program.cs
@@ -5,8 +5,21 @@ namespace Ex2T3_minMaxVector
     class Program
     {
         static void Main(string[] args)
-        {
-            
+         {
+            int[] v = new int[] { 15, 6, 17,19,25,3,2,6,5,89,78,35 };
+            int min = v[0];
+            int max = v[0];
+                for (int i = 0; i < v.Length; i++) 
+                { 
+                    if (v[i] < min)
+                    min = v[i];
+                   if (v[i] > max)
+                    max = v[i];
+            }
+                Console.WriteLine(min);
+                Console.WriteLine(max);
+
+
         }
     }
 }
---
+++ Tema3/Ex3T3_VectoNegPosition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex3T3_VectoNegPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
