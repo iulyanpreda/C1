3535ded78bd6f8e891bbf6f74a93f729bc933652
Iulian Preda <iulyanpreda@yahoo.com>
2020-02-23 19:41:30 +0200

    z

---
+++ Tema3/Ex6T3_intPlusDouble/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex6T3_intPlusDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 4, 5, 6, 7, 8, 9, 23, 45, 68, 79 };
+            double d = 2.5;
+            double[] dv = new double[v.Length];
+            for (int i = 0; i < v.Length; i++)
+            {
+                dv[i] = v[i] + d;
+                Console.WriteLine(dv[i]);
+            }
+        }
+    }
+}
+
